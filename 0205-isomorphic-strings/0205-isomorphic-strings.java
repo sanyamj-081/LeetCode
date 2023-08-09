91271b02500eecb567b3f063a86139d18bd02fab
Sanyam Jain <104319672+sanyamj-081@users.noreply.github.com>
2023-08-09 19:27:16 +0530

    Time: 14 ms (35.26%), Space: 41.2 MB (96.79%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -9,16 +9,15 @@ class Solution {
         for(int i=0;i<s.length();i++) {
             if(!ss.containsKey(s.charAt(i)))
                 ss.put(s.charAt(i),i); 
-        }
-        for(int i=0;i<t.length();i++) {
+            
             if(!tt.containsKey(t.charAt(i)))
                 tt.put(t.charAt(i),i);
-        }
-        for(int i=0;i<s.length();i++) {
-            if(ss.get(s.charAt(i)) != tt.get(t.charAt(i)))
+            
+             if(ss.get(s.charAt(i)) != tt.get(t.charAt(i)))
                 return false;
                 
         }
+        
         return true;
         
         
