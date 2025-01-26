48178028781e9240fad9c5864cffbfc58fc7716b
Sanyam Jain <104319672+sanyamj-081@users.noreply.github.com>
2025-01-26 16:41:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Zero Converter - Python/zero-converter-python.py
@@ -0,0 +1,42 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+def pos(n):
+    ## Write the code
+    for i in range(1,n+1):
+        print (n-i,end = " ")
+        
+    
+def neg(n):
+    ##Write the code
+    for i in range(n,1):
+        print(i,end = " ")
+    
+
+#{ 
+ # Driver Code Starts.
+
+
+def main():
+    testcases=int(input()) #testcases
+    while(testcases>0):
+        n = int(input())
+        if(n > 0):
+            pos(n)
+        elif(n < 0):
+            neg(n)
+        else:
+            print("already Zero",end="")
+        print()
+        testcases-=1
+        
+
+
+        print("~")
+if __name__=='__main__':
+    main()
+# } Driver Code Ends
